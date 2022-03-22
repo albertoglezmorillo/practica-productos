fba8dbdf8fd077c3afe9a53f5123cd24b976c842
Alfonso <alfonsoderojasperez@gmail.com>
2022-03-22 10:03:43 +0100

    componente imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
-<<<<<<< HEAD
 import { ImagenesComponent } from './imagenes/imagenes.component';
-=======
 import { FiltroComponent } from './filtro/filtro.component';
->>>>>>> d3513d207870ae6c742fd6faedcd881367caaeb2
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-<<<<<<< HEAD
-    ImagenesComponent
-=======
+    ImagenesComponent,
     FiltroComponent
->>>>>>> d3513d207870ae6c742fd6faedcd881367caaeb2
   ],
   imports: [
     BrowserModule
