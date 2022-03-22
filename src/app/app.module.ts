d3513d207870ae6c742fd6faedcd881367caaeb2
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-03-22 09:56:01 +0100

    creamos componenete de filtro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { FiltroComponent } from './filtro/filtro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    FiltroComponent
   ],
   imports: [
     BrowserModule
